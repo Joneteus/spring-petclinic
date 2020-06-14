1559f67445e3249bbaaee065ed76dc6c42a7f3e9
Joni Ollikainen <joni.ollikainen@gofore.com>
2020-06-14 11:39:37 +0300

    Added ECS task exec role

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ provider aws {
   region  = var.aws_region
 }
 
+# Networking
 resource aws_default_subnet subnet-a {
   availability_zone = join("", [var.aws_region, "a"])
 }
@@ -18,6 +19,7 @@ resource aws_default_subnet subnet-c {
   availability_zone = join("", [var.aws_region, "c"])
 }
 
+# Base resources for ECS
 resource aws_ecr_repository joneteus-spring-petclinic {
   name = "joneteus-spring-petclinic"
 }
@@ -28,4 +30,25 @@ resource aws_cloudwatch_log_group joneteus-spring-petclinic-logs {
 
 resource aws_ecs_cluster joneteus-spring-petclinic {
   name = "joneteus-spring-petclinic"
+}
+
+# ECS Task execution role
+data aws_iam_policy_document joneteus-spring-petclinic-assume {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource aws_iam_role_policy_attachment aws-ecs-task-exec-attach {
+  role       = aws_iam_role.joneteus-spring-petclinic-ecs-task-exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource aws_iam_role joneteus-spring-petclinic-ecs-task-exec {
+  name = "joneteus-spring-petclinic-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.joneteus-spring-petclinic-assume.json
 }
