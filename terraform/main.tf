e854dd59c49499d18bf24044c7d3ead7ecfbca71
Joni Ollikainen <joni.ollikainen@gofore.com>
2020-06-14 16:46:08 +0300

    Created load balancing configuration for ecs app

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,14 +128,7 @@ resource aws_security_group joneteus-spring-petclinic-alb-sg {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["88.148.236.57/32"]
   }
 }
 
@@ -149,6 +142,25 @@ resource aws_lb joneteus-spring-petclinic-alb {
     aws_subnet.public-subnet-b.id, 
     aws_subnet.public-subnet-c.id
   ]
+}
+
+resource aws_lb_listener joneteus-spring-petclinic-http  {
+  load_balancer_arn = aws_lb.joneteus-spring-petclinic-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.joneteus-spring-petclinic-tg.arn}"
+  }
+}
+
+resource aws_lb_target_group joneteus-spring-petclinic-tg {
+  name     = "joneteus-spring-petclinic-alb-tg"
+  port     = 8080
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = aws_vpc.joneteus-spring-petclinic-vpc.id
 }
 
 # Base resources for ECS
@@ -201,6 +213,12 @@ resource aws_ecs_task_definition joneteus-spring-petclinic {
     "essential": true,
     "cpu": 512,
     "memory": 1024,
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080
+      }
+    ],
     "logConfiguration": {
       "logDriver": "awslogs",
       "options": {
@@ -221,20 +239,12 @@ resource aws_security_group joneteus-spring-petclinic-ecs-sg {
   vpc_id      = aws_vpc.joneteus-spring-petclinic-vpc.id
 
   ingress {
-    description = "HTTP 8080 from home"
+    description = "HTTP 8080 from ALB"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["88.148.236.57/32"]
+    security_groups = [aws_security_group.joneteus-spring-petclinic-alb-sg.id]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 }
 
 resource aws_ecs_service joneteus-spring-petclinic {
@@ -252,5 +262,11 @@ resource aws_ecs_service joneteus-spring-petclinic {
       aws_subnet.private-subnet-b.id,
       aws_subnet.private-subnet-c.id
       ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.joneteus-spring-petclinic-tg.arn
+    container_name   = "joneteus-spring-petclinic"
+    container_port   = 8080
   }
 }
