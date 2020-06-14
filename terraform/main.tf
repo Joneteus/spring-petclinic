8598e81b376d45441b859769fee43a9c7697ad9a
Joni Ollikainen <joni.ollikainen@gofore.com>
2020-06-14 12:34:22 +0300

    Created ECS service

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,9 @@ provider aws {
 }
 
 # Networking
+resource aws_default_vpc default {
+}
+
 resource aws_default_subnet subnet-a {
   availability_zone = join("", [var.aws_region, "a"])
 }
@@ -17,6 +20,28 @@ resource aws_default_subnet subnet-b {
 
 resource aws_default_subnet subnet-c {
   availability_zone = join("", [var.aws_region, "c"])
+}
+
+resource aws_security_group joneteus-spring-petclinic-ecs {
+  name        = "joneteus-spring-petclinic-ecs"
+  description = "Security group for joneteus-spring-petclinic ECS application"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "HTTP 8080 from home"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["88.148.236.57/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 # Base resources for ECS
@@ -80,4 +105,22 @@ resource aws_ecs_task_definition joneteus-spring-petclinic {
   }
 ]
 EOF
+}
+
+# ECS Service
+resource aws_ecs_service joneteus-spring-petclinic {
+  name = "joneteus-spring-petclinic-service"
+  cluster = aws_ecs_cluster.joneteus-spring-petclinic.id
+  task_definition = aws_ecs_task_definition.joneteus-spring-petclinic.arn
+  launch_type = "FARGATE"
+  desired_count = 1
+  network_configuration {
+    assign_public_ip = true
+    security_groups = [ aws_security_group.joneteus-spring-petclinic-ecs.name ]
+    subnets = [
+      aws_default_subnet.subnet-a.id,
+      aws_default_subnet.subnet-b.id,
+      aws_default_subnet.subnet-c.id
+      ]
+  }
 }
