8eed891c116a939a4876c4ac63c3d840a6f389da
Joni Ollikainen <joni.ollikainen@gofore.com>
2020-06-14 10:34:22 +0300

    Fixed codebuild source env

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+variable aws_region {}
+
+
+provider aws {
+  version = "~> 2.0"
+  region  = var.aws_region
+}
+
+resource aws_default_subnet subnet-a {
+  availability_zone = join("", [var.aws_region, "a"])
+}
+
+resource aws_default_subnet subnet-b {
+  availability_zone = join("", [var.aws_region, "b"])
+}
+
+resource aws_default_subnet subnet-c {
+  availability_zone = join("", [var.aws_region, "c"])
+}
