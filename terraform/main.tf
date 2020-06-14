f75f29db32b1ac5222eeca4b4bc48f01974244ce
Joni Ollikainen <joni.ollikainen@gofore.com>
2020-06-14 11:16:00 +0300

    Added more resources to tf stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,15 @@ resource aws_default_subnet subnet-b {
 resource aws_default_subnet subnet-c {
   availability_zone = join("", [var.aws_region, "c"])
 }
+
+resource aws_ecr_repository joneteus-spring-petclinic {
+  name = "joneteus-spring-petclinic"
+}
+
+resource aws_cloudwatch_log_group joneteus-spring-petclinic-logs {
+  name = "joneteus-spring-petclinic"
+}
+
+resource aws_ecs_cluster joneteus-spring-petclinic {
+  name = "joneteus-spring-petclinic"
+}
